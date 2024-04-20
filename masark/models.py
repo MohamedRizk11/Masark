dc409ea0b76b32e4eb0505aaef62d710954aa315
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2024-04-20 23:17:34 +0200

    add str

--- masark/models.py
+++ masark/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 Status_type=(
     ("Normal","Normal"),
@@ -8,18 +9,23 @@ Status_type=(
 )
 
 class Users(models.Model):
-    ID=models.IntegerField(max_length=30)
-    status=models.CharField(max_length=10)
+    ID=models.IntegerField(_("ID"),max_length=30)
+    Status=models.CharField(_("Status"),max_length=10,choices=Status_type)
+
+    def __str__(self):
+        return self.ID
 
 
 class Station(models.Model):
-    ID=models.IntegerField(max_length=30)
-    Name=models.CharField(max_length=20)
-    FamousPlaces=models.CharField(max_length=50)
-    Tickets=models.ForeignKey("Ticket",related_name=("station_ticket"), on_delete=models.CASCADE)
+    ID=models.IntegerField(_("ID"),max_length=30)
+    Name=models.CharField(_("Name"),max_length=20)
+    FamousPlaces=models.CharField(_("FamousPlaces"),max_length=50)
+    def __str__(self):
+        return self.Name    
 
 class Ticket(models.Model):
-    ID=models.IntegerField(max_length=30)
-    StationID=models.IntegerField(max_length=30)
-    Cost=models.FloatField()
-    Time=models.FloatField()    
+    StationID=models.ForeignKey("station",verbose_name=_("StationID"),related_name="ticket_station", on_delete=models.SET_NULL,null=True)
+    Cost=models.FloatField(_("Cost"),)
+    Time=models.FloatField(_("Time"),)    
+    def __str__(self):
+        return self.StationID
