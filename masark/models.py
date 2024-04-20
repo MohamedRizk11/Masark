cacb449dc9725851ed9922131cbf0acd3f959217
MohamedRizk11 <mohamedrizk1222211@gmail.com>
2024-04-20 22:57:22 +0200

    desgin models

--- masark/models.py
+++ masark/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+Status_type=(
+    ("Normal","Normal"),
+    ("Disabled","Disabled"),
+
+)
+
+class Users(models.Model):
+    ID=models.IntegerField(max_length=30)
+    status=models.CharField(max_length=10)
+
+
+class Station(models.Model):
+    ID=models.IntegerField(max_length=30)
+    Name=models.CharField(max_length=20)
+    FamousPlaces=models.CharField(max_length=50)
+    Tickets=models.ForeignKey("Ticket",related_name=("station_ticket"), on_delete=models.CASCADE)
+
+class Ticket(models.Model):
+    ID=models.IntegerField(max_length=30)
+    StationID=models.IntegerField(max_length=30)
+    Cost=models.FloatField()
+    Time=models.FloatField()    
